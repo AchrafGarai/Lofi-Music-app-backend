3cfad04351824e07a47cc91c2119c7a95b62363d
AchrafGarai <32675431+AchrafGarai@users.noreply.github.com>
2022-05-10 12:46:06 +0100

    My first commit

--- src/queries/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as getAllPlaylistsQuery } from './get-playlist-many-query'
-export { default as getTopArtists } from './get-artists-many-query'
-export { default as getPlaylistQuery } from './get-playlist-query'
